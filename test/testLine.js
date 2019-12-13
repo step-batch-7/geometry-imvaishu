c59fef4157b66b0e6481f5acfa7419ffd4f7e13d
vaishnavi <58027647+imvaishu@users.noreply.github.com>
2019-12-13 14:28:35 +0530

    modified unit test cases

--- test/testLine.js
+++ test/testLine.js
@@ -3,39 +3,39 @@ Line = require("../src/line.js");
 
 describe("Line", function() {
   it("should return toString", function() {
-    let actualValue = new Line({ x: 1, y: 4 }, { x: 3, y: 9 });
-    actualValue = actualValue.toString();
+    let actualValue = new Line({ x: 1, y: 4 }, { x: 3, y: 9 }).toString();
     const expectedValue = "Line (1,4) (3,9)";
+
     assert.deepStrictEqual(actualValue, expectedValue);
   });
 });
 
 describe("isEqualTO", function() {
   it("should return true if lines are equal ", function() {
-    let actualValue = new Line({ x: 1, y: 4 }, { x: 3, y: 9 });
-    const expectedValue = new Line({ x: 1, y: 4 }, { x: 3, y: 9 });
-    actualValue = actualValue.isEqualTo(expectedValue);
-    assert.strictEqual(actualValue, true);
+    const line = new Line({ x: 1, y: 4 }, { x: 3, y: 9 });
+    const otherLine = new Line({ x: 1, y: 4 }, { x: 3, y: 9 });
+
+    assert.strictEqual(line.isEqualTo(otherLine), true);
   });
 
   it("should return false if both are lines and points are not equal", function() {
-    let actualValue = new Line({ x: 2, y: 3 }, { x: 4, y: 7 });
-    const expectedValue = new Line({ x: 1, y: 4 }, { x: 3, y: 9 });
-    actualValue = actualValue.isEqualTo(expectedValue);
-    assert.strictEqual(actualValue, false);
+    const line = new Line({ x: 2, y: 3 }, { x: 4, y: 7 });
+    const otherLine = new Line({ x: 1, y: 4 }, { x: 3, y: 9 });
+
+    assert.strictEqual(line.isEqualTo(otherLine), false);
   });
 
   it("should return false if one is not line and points are equal", function() {
-    let actualValue = new Line({ x: 2, y: 3 }, { x: 4, y: 7 });
-    const expectedValue = { endA: { x: 2, y: 3 }, endB: { x: 4, y: 7 } };
-    actualValue = actualValue.isEqualTo(expectedValue);
-    assert.strictEqual(actualValue, false);
+    const line = new Line({ x: 2, y: 3 }, { x: 4, y: 7 });
+    const otherObject = { endA: { x: 2, y: 3 }, endB: { x: 4, y: 7 } };
+
+    assert.strictEqual(line.isEqualTo(otherObject), false);
   });
 
   it("should return false if one is not line and points are not equal", function() {
-    let actualValue = new Line({ x: 2, y: 3 }, { x: 4, y: 7 });
-    const expectedValue = { endA: { x: 1, y: 4 }, endB: { x: 3, y: 9 } };
-    actualValue = actualValue.isEqualTo(expectedValue);
-    assert.strictEqual(actualValue, false);
+    const line = new Line({ x: 2, y: 3 }, { x: 4, y: 7 });
+    const otherObject = { endA: { x: 1, y: 4 }, endB: { x: 3, y: 9 } };
+
+    assert.strictEqual(line.isEqualTo(otherObject), false);
   });
 });
